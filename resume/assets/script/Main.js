7653b94cc230b944f73a729df50d27b3af730cbe
WEIMOWANG <WEIMOWANG@WEIMOWANGde-MacBook-Pro.local>
2019-01-08 16:53:06 +0800

    add the animation form anime

--- resume/assets/script/Main.js
+++ resume/assets/script/Main.js
@@ -28,7 +28,7 @@
         event.preventDefault();
         window.open('https://itunes.apple.com/tw/app/powercloud/id989675113?mt=8');
     })
-    
+
     $('#travelandroid').click(function (event) {
         event.preventDefault();
         window.open('https://play.google.com/store/apps/details?id=com.weimowang.myapplication');
@@ -53,5 +53,43 @@
         event.preventDefault();
         window.open('https://itunes.apple.com/tw/app/帳單小幫手/id1244607677?mt=8');
     })
-    
+
+    anime.timeline({ loop: false })
+        .add({
+            targets: '.weiheader .word',
+            scale: [14, 1],
+            opacity: [0, 1],
+            easing: "easeOutCirc",
+            duration: 800,
+            delay: function (el, i) {
+                return 800 * i;
+            }
+        }).add({
+            targets: '.weiheader',
+            opacity: 1,
+            duration: 1000,
+            easing: "easeOutExpo",
+            delay: 1000
+        });
+
+
+    $('.myheader').each(function () {
+        $(this).html($(this).text().replace(/([^\x00-\x80]|\w)/g, "<span class='letter'>$&</span>"));
+    });
+    anime.timeline({ loop: false })
+        .add({
+            targets: '.myheader .letter',
+            translateY: [-100, 0],
+            easing: "easeOutExpo",
+            duration: 1400,
+            delay: function (el, i) {
+                return 30 * i;
+            }
+        }).add({
+            targets: '.myheader',
+            opacity: 1,
+            duration: 1000,
+            easing: "easeOutExpo",
+            delay: 1000
+        });
 })(jQuery);
