ef39dfe98f09372f101af3964e82865d72cf7f81
WEIMOWANG <WEIMOWANG@WEIMOWANGde-MacBook-Pro.local>
2019-11-23 18:04:49 +0800

    update rwd for phone

--- resume/assets/scripts/Main.js
+++ resume/assets/scripts/Main.js
@@ -1,17 +1,23 @@
 window.onload = function () {
 
-    //to set the progress value
+    /**
+     * to set the progress value
+     */
     function progress_animate() {
         $('.wrappertwo-content').find('.progress-bar').each(function () {
             $(this).css('width', $(this).attr('aria-valuenow') + '%');
         })
     }
 
+    /**
+    * to set the scroll init
+    */
     function set_scroll_init() {
         var currentscrollTop = $(window).scrollTop();
         var remove_active = function () {
             $('#menu').find('li.active').toggleClass('active');
         }
+
         if (currentscrollTop == $("#wrapperone").offset().top && currentscrollTop < $("#wrappertwo").offset().top - 100) {
             remove_active()
             $('#menu').find('li').eq(0).toggleClass('active');
@@ -38,8 +44,22 @@ window.onload = function () {
         set_scroll_init();
     });
 
-    $('#menu li').click(function () {
-        var _index = $(this).index();
+    //triggle phone menu ul button
+    document.getElementById('menubtn').addEventListener('click', function (e) {
+        $(this).toggleClass('menu-phone-active');
+        $('#menuUl').toggleClass('menu-phone-active');
+
+        //set the li init to first one when user click the menubtn
+        $('#menuUl').find('li.current-active').toggleClass('current-active');
+        $('#menuUl').find('li').eq(0).toggleClass('current-active');
+    })
+
+
+    /**
+     * user clikk btn to the wrapper position 
+     * @param index,int
+     */
+    function toBlockposition(_index) {
         var _item = "";
         switch (_index) {
             case 0:
@@ -55,17 +75,32 @@ window.onload = function () {
                 _item = "#wrapperfour"
                 break;
         }
+
         $('html, body').animate({
             scrollTop: $(_item).offset().top
         }, 600);
 
-        // $(this).parent().find('li').each(function () {
-        //     if ($(this).hasClass('active')) {
-        //         $(this).toggleClass('active');
-        //     }
-        // })
-        // $(this).toggleClass('active');
+    }
 
+    //triggle phone size  menu ul buttons
+    $('#menuUl li').click(function () {
+        var _index = $(this).index();
+        $(this).parent().find('li').each(function () {
+            if ($(this).hasClass('current-active')) {
+                $(this).toggleClass('current-active');
+            }
+        })
+        $(this).toggleClass('current-active');
+        //close the menu ul 
+        $('#menuUl').toggleClass('menu-phone-active');
+        toBlockposition(_index);
+
+    })
+
+    //triggle web size  menu ul buttons
+    $('#menu li').click(function () {
+        var _index = $(this).index();
+        toBlockposition(_index);
     })
 
 
